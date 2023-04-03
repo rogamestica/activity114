ee74b076d870020637dd40b29971d04ec5ddf733
rogamestica <rogamestica.pascual815@gmail.com>
2023-04-03 09:22:36 +0800

    add components greeting

---
+++ app/src/components/greeting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Greeting(props) {
+    return (
+      <div>
+        <h1>Hello {props.name}!</h1>
+      </div>
+    );
+  }
+
+  export default Greeting;
+
+  
