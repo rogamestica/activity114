ce6c786f3944f13e5a9440077a92bf0e879a8817
rogamestica <rogamestica.pascual815@gmail.com>
2023-04-06 09:36:36 +0800

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/greeting';
-import Counter from './components/counter';
+// import Greeting from './components/greeting';
+// import Counter from './components/counter';
 import Person from './components/Person';
 import PersonList from './components/personlist';
 
@@ -17,10 +16,13 @@ function App() {
       <Greeting name="Sarah" />
 
       <Counter /> */}
-     
-      <Person/>
 
       < PersonList />
+
+      <Person name="John" age="30" />
+     <Person name="Mary" age="25" />
+     <Person name="Alex" age="40" />
+     <Person name="Sarah" age="20" /> 
 
     </div>
   );
--- app/src/components/person.jsx
+++ app/src/components/person.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 function Person(props) {
     return (
-        <div>
+        <div className='table container'>
+            <table>
             <h2>{props.name}</h2>
             <p>{props.age}</p>
-
-            <Person name="John" age="30" />
-            <Person name="Mary" age="25" />
-            <Person name="Alex" age="40" />
-            <Person name="Sarah" age="20" /> 
+            </table>
         </div>
     );
 }
